4e3284d34cf4c66ec0354c0018f6cfee9a475426
jessonziegler <jziegleraudio@gmail.com>
2022-08-11 22:41:15 -0400

    added buttons and changed media queries

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -119,7 +119,7 @@ const Footer = () => {
         </div>
       ) : (
         <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
+          <h3 className="head-text">Thanks!</h3>
         </div>
       )}
     </>
--- frontend_react/src/container/Projects/Projects.jsx
+++ frontend_react/src/container/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/themes/theme-bojack.css";
 
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
@@ -77,7 +79,7 @@ const Projects = () => {
                 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={work.projectLink} target="_self" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -87,7 +89,8 @@ const Projects = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+
+                <a href={work.codeLink} target="_self" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -105,7 +108,24 @@ const Projects = () => {
               <p className="p-text" style={{ marginTop: 10 }}>
                 {work.description}
               </p>
-
+              <div id="buttondiv">
+                <AwesomeButton
+                  className="buttonpro"
+                  type="primary"
+                  size="small"
+                  href={work.projectLink}
+                >
+                  Project
+                </AwesomeButton>
+                <AwesomeButton
+                  className="buttonpro"
+                  type="primary"
+                  size="small"
+                  href={work.codeLink}
+                >
+                  Code
+                </AwesomeButton>
+              </div>
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>
